d7e2da46e2698573758325e02107385532fe35fd
Bada1208 <sysoievbogdan@gmail.com>
2020-02-20 05:58:09 +0100

    data sampling(выборка данных)

--- src/main/java/com/sysoiev/hibernate/Start.java
+++ src/main/java/com/sysoiev/hibernate/Start.java
@@ -1,5 +1,7 @@
 package com.sysoiev.hibernate;
 
+import com.sysoiev.hibernate.entity.Author;
+import com.sysoiev.hibernate.entity.Book;
 import org.hibernate.Session;
 
 public class Start {
@@ -7,7 +9,16 @@ public class Start {
 
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
+        for (Author author : new AuthorHelper().getAuthorList()
+        ) {
+            System.out.println("author = " + author.getName());
 
+        }
+        for (Book book : new BookHelper().getBookList()
+        ) {
+            System.out.println("book = " + book.getName());
+
+        }
         HibernateUtil.getSessionFactory().close(); // закрываем фабрику, иначе программа останется в "зависнутом состоянии"
 
 
